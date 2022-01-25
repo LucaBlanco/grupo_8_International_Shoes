b2e02cd71aa33f16c953a0facdb581a0ede2c5f3
Juan <juanm.fernandez@hotmail.com>
2022-01-25 15:48:26 -0300

    Cambio parcial ejs y reestructuracion de algunas carpetas

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express=require("express");
-
 const path=require('path');
-
 const app= express();
 
 const publicPath=path.resolve(__dirname,'./public');
 
 app.use( express.static(publicPath) );
 
-app.listen(5000, ()=> {console.log('Servidor corriendo en el puerto 5000');
-});
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, './views'));
+
+app.listen(5000, ()=> {console.log('Servidor corriendo en el puerto 5000')});
 
 app.get('/',(req, res)=>{
     let rutaIndex= path.resolve('./views/home.html');
@@ -26,11 +26,19 @@ app.get('/registro',(req, res)=>{
 })
 
 app.get('/carrito',(req, res)=>{
-    let rutaIndex= path.resolve('./views/carrito.html');
-    res.sendFile(rutaIndex);
+    let rutaIndex= path.resolve('./src/views/product/carrito');
+    res.render(rutaIndex);
+})
+app.get('/producto',(req, res)=>{
+    let rutaIndex= path.resolve('./src/views/product/detalle_producto');
+    res.render(rutaIndex);
 })
 
-app.get('/producto',(req, res)=>{
-    let rutaIndex= path.resolve('./views/productDetail.html');
-    res.sendFile(rutaIndex);
+app.get('/crear',(req, res)=>{
+    let rutaIndex= path.resolve('./src/views/product/crear');
+    res.render(rutaIndex);
+})
+app.get('/editar',(req, res)=>{
+    let rutaIndex= path.resolve('./src/views/product/editar');
+    res.render(rutaIndex);
 })
