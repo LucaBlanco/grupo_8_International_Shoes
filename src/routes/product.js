2196683a9165cf3b9139e8c44c3ad20e6de3566f
Juan <juanm.fernandez@hotmail.com>
2022-02-18 02:21:33 -0300

    Subida de imagenes de producto mejoradas

--- src/routes/product.js
+++ src/routes/product.js
@@ -4,34 +4,26 @@ const {index, show, nuevo, storage, editar, modify, trash, details} = require('.
 
 const multer = require('multer');
 const folder = require('../middlewares/storage');
-const upload = multer({storage: folder()})
+const req = require('express/lib/request');
+const upload = multer({storage: folder()}).single('imagen')
 
 /** aca debe ir el index de producto, es decir, su listado, no confundir con el index del sitio 
  *  router.get('/', index) <-- index de producto => [http://localhost:3030/productos]
 */
 
 router.get('/nuevo', nuevo)
-router.post('/guardar', [upload.any()], storage)
+router.post('/guardar', upload, storage);
 
 router.get('/editar/:id', editar)
-router.put('/actualizar', [upload.any()], modify)
+router.put('/actualizar', upload, modify)
 
 router.get('/:id', details)
 
 /* router.get('/:id', show) */
 
 /*
-
 router.get('/', index)
-
 router.get('/:id', show)
-router.get('/editar/:id', editar)
-
-
-router.post('/guardar', [upload.any()], storage)
-
-
-
 router.delete('/borrar', trash)
 */
 module.exports = router;
