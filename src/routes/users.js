59cf37fced14241ce641aebb868cbf327ef7b910
Juan <juanm.fernandez@hotmail.com>
2022-03-06 23:16:00 -0300

    Sesiones login merge 2

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,12 +7,12 @@ const {login, registro, auth, perfil} = require('../controllers/users');
 const {index,deposit}= require('../controllers/user_register');
 const router = Router();
 
-const upload=multer({storage});
+//const upload=multer({storage});
 
 router.get('/login', login);
 
 router.get('/registro', index)
-router.post('/registro/guardar',upload.single('imagenUsuario'),deposit);
+router.post('/registro/guardar', /*upload.single('imagenUsuario'),*/ deposit);
 
 router.post('/auth', auth)
 router.get('/perfil', perfil)
