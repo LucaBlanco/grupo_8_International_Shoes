99441e417b7c51880c9f01d6c3a8f847e5bf07b3
Juan <juanm.fernandez@hotmail.com>
2022-03-06 23:10:47 -0300

    Sesiones login merge

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ const multer=require('multer');
 
 const {login, registro, auth, perfil} = require('../controllers/users');
 
-const {login, auth} = require('../controllers/users');
+//const {login, auth} = require('../controllers/users');
 const {index,deposit}= require('../controllers/user_register');
 const router = Router();
 
