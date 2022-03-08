413908bf06169df88cb65bdd755ce6025e647aca
Juan <juanm.fernandez@hotmail.com>
2022-03-08 16:36:29 -0300

    Ajustes css

--- src/app.js
+++ src/app.js
@@ -6,18 +6,20 @@ const cookies = require('cookie-parser');
 
 const app= express();
 
+const isLoggedMidle = require('./middlewares/isLoggedMidle');
+
 const publicPath= resolve(__dirname,'../public');
 app.use( express.static(publicPath) );
 
-const isLoggedMidle = require('./middlewares/isLoggedMidle');
+
 app.use(session({
     secret: 't[%H+>mg`GKDG4F$',
     resave: false,
     saveUninitialized: false
 }));
-app.use(isLoggedMidle);
 
 app.use(cookies());
+app.use(isLoggedMidle);
 
 app.set('view engine', 'ejs');
 app.set('views',  resolve(__dirname, './views'));
--- src/middlewares/isLoggedMidle.js
+++ src/middlewares/isLoggedMidle.js
@@ -3,12 +3,15 @@ const User = require('../models/users');
 function isLoggedMidle(req, res, next) {
     res.locals.isLogged = false;
 
-/*
+
     let emailCookie = req.cookies.userEmail;
-    let userFromCookie = User.match('email', emailCookie);        
-    req.session.usuarioLogueado = userFromCookie;
-    console.log(`Vino del midle: ${req.cookies}`);
-*/
+    let userFromCookie = User.match('email', emailCookie); 
+    if (userFromCookie) {
+        req.session.usuarioLogueado = userFromCookie;        
+    }       
+
+    console.log(`Vino del midle: ${req.cookies.userEmail}`);
+    console.table(userFromCookie);
 
     if (req.session && req.session.usuarioLogueado) {
         res.locals.isLogged = true;
